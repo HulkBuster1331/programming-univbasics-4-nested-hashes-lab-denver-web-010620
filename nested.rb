36589f40198965518de4e19334c0bba4a9804371
Dillon Franklin <drfranklin1331@gmail.com>
2019-12-19 18:43:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,10 +82,10 @@ def adding_matz
      }
   programmer_hash[:yukihiro_matsumoto] = {}
   
-    programmer_hash: 
+  programmer_hash: 
     yukihiro_matsumoto:,
-   programmer_hash [known_for:] "Ruby",
-    :languages=> ["LISP", "C"]
+     programmer_hash [known_for:] "Ruby",
+      programmer_hash [languages:] ["LISP", "C"]
   }
   
     
